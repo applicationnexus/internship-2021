ee8d9cfaec2ada6494eb29b85532bbba44e509a1
yuvrajd9597 <65776902+yuvrajd9597@users.noreply.github.com>
2021-10-23 18:31:22 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+function myFunction(){
+  var a = document.getElementById("rupees").value;
+  var x =document.getElementById("country").value;
+  var o;
+  if (a ==null || a == undefined || a.length==0)
+  {
+    alert("Please enter a value..")
+  }
+  else{
+  switch(x)
+  {
+    case "USD":
+      o = a*74.99;
+      alert("The Converted Currency is:\n" +o);
+      break;
+
+    case "EUR":
+      o = a*87.32;
+      alert("The Converted Currency is:\n" +o);
+      break;
+      
+    case "AUD":
+      o = a*55.97;
+      break;
+      
+    case "JPY":
+      o = a*0.66;
+      alert("The Converted Currency is:\n" +o);
+      break;
+      
+    case "CAD":
+    o = a*60.65;
+    alert("The Converted Currency is:\n" +o);
+        break;
+  }
+}
+  }
